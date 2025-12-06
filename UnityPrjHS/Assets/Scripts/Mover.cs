7d77872b29f4ebbc513eb9f48074253f3cbeffa5
Oksusu <gustn3946@naver.com>
2025-12-06 22:49:09 +0900

    addtion study
    deltatime and cinemachine,
    There are some changes in cinemachine with our lecture.
    We gotta find "Trackong Target".

--- UnityPrjHS/Assets/Scripts/Mover.cs
+++ UnityPrjHS/Assets/Scripts/Mover.cs
@@ -4,7 +4,7 @@ using UnityEngine.UIElements;
 public class Mover : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    [SerializeField] float MoveSpeed = 3f;
     void Start()
     {
         
@@ -13,8 +13,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xValue = Input.GetAxis("Horizontal");
-        float zValue = Input.GetAxis("Vertical");
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * MoveSpeed;
         float yValue = 0f;
         transform.Translate(xValue, yValue, zValue);
     }
